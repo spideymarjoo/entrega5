95f5de757e734fb5ce1db40244bc35e82b439286
DESKTOP-L7H1FM4\Usuario <marjo>
2025-01-29 12:28:55 +0100

    fichero entrenador, creacion de atributos y metodo constructor

---
+++ tarea5/src/futbol/entrenador/entrenador.java
@@ -0,0 +1,11 @@
+package futbol.entrenador;
+
+public class entrenador {
+    private String nombre;
+    private String formacionPreferida;
+
+    public entrenador(String nombre, String formacionPreferida, String dni) {
+        this.nombre = nombre;
+        this.formacionPreferida = formacionPreferida;
+    }
+}
