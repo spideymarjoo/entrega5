52dd53e0525d8d9f5d361ca0e1dc066d99e864f2
SISTEMASNINF\majun <a>
2025-02-10 00:52:10 +0100

    arreglo de main

--- tarea5/src/App.java
+++ tarea5/src/App.java
@@ -6,6 +6,7 @@ import futbol.jugador.Posicion;
 import futbol.jugador.Traspaso;
 import futbol.jugador.jugador;
 import futbol.presidente.presidente;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -17,16 +18,16 @@ public class App {
     }
     public static void main(String[] args) throws Exception {
     
-        jugador j1 = new jugador( "Lamine Yamal", fechanacimiento(2007, 11, 20), "Espania", Posicion.DELANTERO, 10, Traspaso.SIN_SOLICITAR);
-        jugador j2 = new jugador( "Paolo Guerreo", fechanacimiento(1986, 10, 06), "Peru", Posicion.DELANTERO, 9, Traspaso.SIN_SOLICITAR);
-        jugador j3 = new jugador( "Jefferson Farfan", fechanacimiento(1988, 10, 26), "Peru", Posicion.DELANTERO, 10, Traspaso.SIN_SOLICITAR);
-        jugador j4 = new jugador( "Pau Cubarsi", fechanacimiento(2007, 19, 11), "Espania", Posicion.DEFENSA, 8, Traspaso.SIN_SOLICITAR);
-        jugador j5 = new jugador( "Cristiano Ronaldo", fechanacimiento(1984, 6, 29), "Portugal", Posicion.DELANTERO, 7, Traspaso.SIN_SOLICITAR);
-        jugador j6 = new jugador( "Leo Messi", fechanacimiento(1986, 9, 11), "Argentina", Posicion.DELANTERO, 10, Traspaso.SIN_SOLICITAR);
-        jugador j7 = new jugador( "Kylyan Mbappe", fechanacimiento(1998, 10, 17), "Francia", Posicion.DELANTERO, 8, Traspaso.SIN_SOLICITAR);
-        jugador j8 = new jugador( "Cristian Cueva", fechanacimiento(1991, 6, 28), "Peru", Posicion.CENTROCAMPISTA, 6, Traspaso.SIN_SOLICITAR);
-        jugador j9 = new jugador( "Yosimar Yotun", fechanacimiento(1992, 12, 21), "Peru", Posicion.CENTROCAMPISTA, 17, Traspaso.SIN_SOLICITAR);
-        jugador j10 = new jugador( "Pedro Gallese", fechanacimiento(1992, 2, 30), "Peru", Posicion.PORTERO, 1, Traspaso.SIN_SOLICITAR);
+        jugador j1 = new jugador( "Lamine Yamal", fechanacimiento(2007, 11, 20), "Espania", Posicion.DELANTERO, 10, Traspaso.SIN_SOLICITAR, null);
+        jugador j2 = new jugador( "Paolo Guerreo", fechanacimiento(1986, 10, 06), "Peru", Posicion.DELANTERO, 9, Traspaso.SIN_SOLICITAR, null);
+        jugador j3 = new jugador( "Jefferson Farfan", fechanacimiento(1988, 10, 26), "Peru", Posicion.DELANTERO, 10, Traspaso.SIN_SOLICITAR, null);
+        jugador j4 = new jugador( "Pau Cubarsi", fechanacimiento(2007, 19, 11), "Espania", Posicion.DEFENSA, 8, Traspaso.SIN_SOLICITAR, null);
+        jugador j5 = new jugador( "Cristiano Ronaldo", fechanacimiento(1984, 6, 29), "Portugal", Posicion.DELANTERO, 7, Traspaso.SIN_SOLICITAR, null);
+        jugador j6 = new jugador( "Leo Messi", fechanacimiento(1986, 9, 11), "Argentina", Posicion.DELANTERO, 10, Traspaso.SIN_SOLICITAR, null);
+        jugador j7 = new jugador( "Kylyan Mbappe", fechanacimiento(1998, 10, 17), "Francia", Posicion.DELANTERO, 8, Traspaso.SIN_SOLICITAR, null);
+        jugador j8 = new jugador( "Cristian Cueva", fechanacimiento(1991, 6, 28), "Peru", Posicion.CENTROCAMPISTA, 6, Traspaso.SIN_SOLICITAR, null);
+        jugador j9 = new jugador( "Yosimar Yotun", fechanacimiento(1992, 12, 21), "Peru", Posicion.CENTROCAMPISTA, 17, Traspaso.SIN_SOLICITAR, null);
+        jugador j10 = new jugador( "Pedro Gallese", fechanacimiento(1992, 2, 30), "Peru", Posicion.PORTERO, 1, Traspaso.SIN_SOLICITAR, null);
 
         presidente presi1 = new presidente("Florentino Perez", "A12B",null);
         presidente presi2 = new presidente("Johan Laporta", "A23E",null);
